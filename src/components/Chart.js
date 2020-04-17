3e996412089c1c476d83b309657d993a3ff3cb58
ajewe <ajewe@utexas.edu>
2020-04-17 14:39:36 -0500

    simplified value and removed unnecessary totalvalue from chart component

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -11,10 +11,9 @@ const Chart = (props) => {
         <div>
             <PieChart style={{ width: '200px' }}
                 data={[
-                    { title: 'Over', value: `${over.length}`, color: '#C13C37' },
-                    { title: 'Under', value: `${under.length}`, color: '#E38627' },
+                    { title: 'Over', value: over.length, color: '#C13C37' },
+                    { title: 'Under', value: under.length, color: '#E38627' },
                 ]}
-                totalValue={`${over.length + under.length}`}
                 label
                 labelStyle={{
                     fill: 'white',
